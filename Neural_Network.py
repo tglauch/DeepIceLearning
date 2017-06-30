23f43e327f35e500a46406f56c04888e3d2fb108
Theo G <theo.glauch@tum.de>
2017-06-30 13:23:22 -0500

    Added shebang line so code can be executed on condor now

--- Neural_Network.py
+++ Neural_Network.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # coding: utf-8
 
 import os
@@ -17,13 +18,13 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 
 
-input_data = np.array(np.load('./charge.npy'))[0:10000]/100 ### Scale to a reasonable input 
-output_data = np.load('./truevals.npy')[0:10000]
+input_data = np.array(np.load('./charge.npy')) ### Scale to a reasonable input 
+output_data = np.load('./truevals.npy')
 
 print 'Shape of Input Data: {}'.format(np.shape(input_data))
 print 'Shape of Output Data: {}'.format(np.shape(output_data))
 
-tvt_ratio=[10,2,1] ##ratio of test validation and test dataset
+tvt_ratio=[20,2,1] ##ratio of test validation and test dataset
 data_len = len(input_data)
 test_end = int(float(tvt_ratio[0])/np.sum(tvt_ratio)*data_len)
 valid_end = int(float(tvt_ratio[1])/np.sum(tvt_ratio)*data_len)+test_end
@@ -48,14 +49,14 @@ test_out = np.log10(np.concatenate(output_data[valid_end+1:data_len-1, 0:1]))
 
 def base_model():
   model = Sequential()
-  model.add(Convolution3D(32, (3,3,3) , padding="same", kernel_initializer="he_normal",input_shape=(21, 21,51,1)))
+  model.add(Convolution3D(16, (3,3,3) , padding="same", kernel_initializer="he_normal",input_shape=(21, 21,51,1)))
   model.add(BatchNormalization())
   model.add(Activation('relu'))
 
-  # model.add(Convolution3D(10, (3,3,3), padding="same", kernel_initializer="he_normal"))
-  # model.add(BatchNormalization())
-  # model.add(Activation('relu'))
-  # model.add(MaxPooling3D((5, 5, 5), padding='same'))
+  model.add(Convolution3D(32, (3,3,3), padding="same", kernel_initializer="he_normal"))
+  model.add(BatchNormalization())
+  model.add(Activation('relu'))
+  model.add(MaxPooling3D((5, 5, 5), padding='same'))
 
   ##possible things to implement
 
@@ -63,10 +64,10 @@ def base_model():
   #model.add(MaxPooling2D((2, 2), border_mode='same'))
 
   model.add(Flatten()) 
-  model.add(Dropout(0.3))
-  model.add(Dense(64,kernel_initializer='normal', activation='relu'))
+  model.add(Dropout(0.4))
+  model.add(Dense(256,kernel_initializer='normal', activation='relu'))
   model.add(BatchNormalization())
-  model.add(Dense(32,kernel_initializer='normal', activation='relu'))
+  model.add(Dense(64,kernel_initializer='normal', activation='relu'))
   model.add(Dense(1, kernel_initializer='normal'))
   print(model.summary())
 
@@ -78,14 +79,14 @@ def base_model():
 # # Training
 # # ----------------------------------------------------------
 
-estimator = KerasRegressor(build_fn=base_model, nb_epoch=20, batch_size=5, verbose=1)
+estimator = KerasRegressor(build_fn=base_model, nb_epoch=20, batch_size=5, verbose=0)
 seed = 7
 np.random.seed(seed)
 
 estimator.fit(np.expand_dims(train, axis=4),train_out, 
               validation_data=(np.expand_dims(valid, axis=4), valid_out),
               callbacks=[keras.callbacks.CSVLogger('./train_hist/history.csv')], 
-              epochs=4, verbose=1)
-estimator.model.save('./train_hist/model.h5')  # save trained network
-res= estimator.predict(np.expand_dims(test, axis=4), verbose=1)
-np.save('./train_hist/model.h5', [res, test_out])
+              epochs=20, verbose=0)
+estimator.model.save('./train_hist/last_model.h5')  # save trained network
+res= estimator.predict(np.expand_dims(test, axis=4), verbose=0)
+np.save('./train_hist/last_model.npy', [res, test_out])
