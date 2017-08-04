f0fe0f7924c44147eb1ff86ad3b2e30e4ac325a6
Theo Glauch <theo.glauch@tum.de>
2017-08-04 16:54:15 +0200

    commit bigbird because of two low GPU memory

--- condor_submit/train_NN_network.py
+++ condor_submit/train_NN_network.py
@@ -49,6 +49,7 @@ def make_slurm(request_gpus, request_memory, addpath, file_location, arguments):
 #SBATCH --error={2}/condor.err\n\
 #SBATCH --output={2}/condor.out\n\
 #SBATCH --workdir={3}\n\
+#SBATCH --exclude=bigbird\n\
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64\n\
 python Neural_Network.py {4} \n'.\
 format(request_gpus, int(request_memory), addpath, file_location, arguments)
