fixed calculation of test-set results. (now rounding output of network)

johannes/updownclassification_3/updown_network.py
@@ -77,7 +77,7 @@ def base_model(model_def):
         for line in f:
             cur_line = line.strip()
             if cur_line == '' and layer != '':
-                add_layer(model, layer, args,kwargs)
+                add_layer(model, layer, args, kwargs)
                 args = []
                 kwargs = dict()
                 layer = ''
@@ -398,7 +398,7 @@ if __name__ == "__main__":
     test_out = np.squeeze(test_out)
     np.save(os.path.join(file_location,'train_hist/{}/{}/test_results.npy'.format(today, project_name)), 
       [res, test_out])
-    correct = np.sum(res == test_out)
+    correct = np.sum(np.round(res) == test_out)
     total = len(res)
     print "{} / {} = {:6.2f}%".format(correct, total, float(correct)/total*100)
   