e6584451028b621d87edac7ed4f0e6a9b7cc042a
jbstettner <joeran.stettner@icecube.wisc.edu>
2017-08-21 08:20:03 -0500

    removed GPU requirements

--- Apply_Model.py
+++ Apply_Model.py
@@ -5,17 +5,35 @@ import os
 import sys
 from configparser import ConfigParser
 import socket
+import argparse
+
+def parseArguments():
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--folder", help="The absolute path to the project file", type=str)
+  parser.add_argument("--final", dest='final', action='store_true')
+  parser.add_argument("--main_config", type=str, help="Config file",
+                      default="default.cfg")
+  parser.add_argument("--version", action="version", version='%(prog)s - Version 1.0')
+  parser.add_argument("--batch_size", dest='batch_size', type=int, default=500)
+  args = parser.parse_args()
+  return args
+
+
 
 print('Running on Hostcomputer {}'.format(socket.gethostname()))
 
+args = parseArguments()
 parser = ConfigParser()
 try:
-  parser.read('config.cfg')
+  parser.read(args.main_config)
 except:
   raise Exception('Config File is missing!!!!')  
-
+print parser.get("Basics","thisfolder")
 backend = parser.get('Basics', 'keras_backend')
 os.environ["KERAS_BACKEND"] = backend
+
+'''
 if backend == 'theano':
     os.environ["THEANO_FLAGS"] = "mode=FAST_RUN,device=gpu,floatX=float32" 
 
@@ -33,7 +51,7 @@ if cuda_path not in os.environ['LD_LIBRARY_PATH'].split(os.pathsep):
   except Exception, exc:
     print 'Failed re-exec:', exc
     sys.exit(1)
-
+'''
 if backend == 'tensorflow':
   print('Run with backend Tensorflow')
   import tensorflow as tf
@@ -47,7 +65,6 @@ import numpy as np
 import theano
 import keras
 from keras.models import Sequential, load_model
-import argparse
 from configparser import ConfigParser
 import h5py
 import shelve
@@ -55,26 +72,12 @@ from functions import generator, base_model, parse_config_file, prepare_input_ou
 import math
 
 
-
-def parseArguments():
-
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--folder", help="The path to the project folder", type=str)
-  parser.add_argument("--final", dest='final', action='store_true')
-  parser.add_argument("--batch_size", dest='batch_size', type=int, default=500)
-  parser.add_argument("--version", action="version", version='%(prog)s - Version 1.0')
-    # Parse arguments
-  args = parser.parse_args()
-  return args
-
 if __name__ == "__main__":
 
 #################### Process Command Line Arguments ######################################
 
-  parser = ConfigParser()
-  parser.read('config.cfg')
   file_location = parser.get('Basics', 'thisfolder')
-  mc_location = './training_data/'
+  mc_location = parser.get("Basics", "mc_path")
 
   args = parseArguments()
   print"\n ############################################"
@@ -85,23 +88,23 @@ if __name__ == "__main__":
   
 #################### Load and Split the Datasets ######################################  
 
-  DATA_DIR = os.path.join(file_location, args.__dict__['folder'])
+  DATA_DIR = args.__dict__['folder']
   print('Make prediction for model in {}'.format(DATA_DIR))
   shelf = shelve.open(os.path.join(DATA_DIR, 'run_info.shlf'))
 
   if shelf['Files']=='all':
-    input_files = os.listdir(os.path.join(file_location, 'training_data/'))
+      input_files = os.listdir(os.path.join(file_location, 'training_data/'))
   else:
-  	input_files = (shelf['Files']).split(':')
+      input_files = (shelf['Files']).split(':')
 
   model_settings, model_def = parse_config_file(os.path.join(DATA_DIR, 'model.cfg'))
   shapes, shape_names, inp_variables, inp_transformations, out_variables, out_transformations = \
      prepare_input_output_variables(os.path.join(mc_location, input_files[0]), model_settings)
   model = base_model(model_def, shapes, shape_names)
   if args.__dict__['final']:
-  	model.load_weightsl(os.path.join(DATA_DIR,'final_network.h5'))
+      model.load_weights(os.path.join(DATA_DIR,'final_network.h5'))
   else:
-  	model.load_weights(os.path.join(DATA_DIR,'best_val_loss.npy'))  	
+      model.load_weights(os.path.join(DATA_DIR,'best_val_loss.npy'))
   res = []
   test_out = []
 
@@ -115,7 +118,7 @@ if __name__ == "__main__":
                 verbose = 1,
                 max_q_size = 2
                 )
- 
+
   mc_truth = [[]*len(out_variables)]
   for i, file_handler in enumerate(file_handlers):
     down = test_inds[i][0]
