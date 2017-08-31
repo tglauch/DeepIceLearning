63d562ab1144d044a5cc655c71c2126c93ff43c3
Theo Glauch <theo.glauch@tum.de>
2017-08-31 10:24:48 -0500

    rename file

--- condor_submit/create_dataset_dagman.py
+++ condor_submit/create_dataset_dagman.py
@@ -14,14 +14,14 @@ def parseArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("--main_config", help="main config file",\
                         type=str ,default='default.cfg')
-    parser.add_argument("--dataset_config", help="dataet config ",\
+    parser.add_argument("--dataset_config", help="dataset config ",\
                         type=str ,default='create_dataset.cfg')
     parser.add_argument("--project", help="The name for the Project",\
                         type=str ,default='some_NN')
     parser.add_argument("--Rescue", help="If true, run in rescue mode ", action="store_true")
+    parser.add_argument("--filesperJob", help="n files per job ", default=-1,\
+                        type=int)
     parser.add_argument("--name", help="Name for the Dagman Files", type=str ,default='create_dataset')
-    parser.add_argument("--num_files", help="number of files",\
-                        type=int, default=-1)
     args = parser.parse_args()
     return args
 
@@ -40,13 +40,12 @@ if __name__ == '__main__':
     print"############################################\n "
 
 Resc=args.__dict__["Rescue"]
-
 PROCESS_DIR = dataset_parser.get("Basics","dagman_folder")
 if not os.path.exists(PROCESS_DIR):
     os.makedirs(PROCESS_DIR)
 
 WORKDIR = PROCESS_DIR+"/jobs/"
-script = main_parser.get("Basics","thisfolder")+"/Create_Data_Files_Diffuse.py"
+script = os.path.join(main_parser.get("Basics","thisfolder"),"Create_Data_Files.py")
 dag_name = args.__dict__["name"]
 dagFile = WORKDIR+"job_{}.dag".format(dag_name)
 submitFile = WORKDIR+"job_{}.sub".format(dag_name)
@@ -55,37 +54,74 @@ if not Resc:
     if not os.path.exists(WORKDIR):
         os.makedirs(WORKDIR)
         print "Created New Folder in: "+ WORKDIR
-    path=PROCESS_DIR+"/logs/{}/".format(dag_name)
-    if not os.path.exists(path):
-        os.makedirs(path)
-        print "Created New Folder in: "+ path
+    log_path=os.path.join(PROCESS_DIR,"logs/{}/".format(dag_name))
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+        print "Created New Folder in: "+ log_path
     print "Write Dagman Files to: "+submitFile
-    arguments = " --project $(PROJECT) --folder $(FOLDER) --main_config $(MAIN)"\
-            +" --dataset_config $(DATASET) --num_files $(NUMFILES) " 
+    arguments = " --project $(PROJECT) --filelist $(PATH) "\
+            +" --dataset_config $(DATASET) "
     submitFileContent = {"universe": "vanilla",
                       "notification": "Error",
                       "log": "$(LOGFILE).log",
                       "output": "$(LOGFILE).out",
                       "error": "$(LOGFILE).err",
                       "request_memory": "1.5GB",
-                      "IWD" : main_parser.get("Basics","thisfolder"),
+                     # "IWD" : main_parser.get("Basics","thisfolder"),
                       "arguments": arguments}
     submitFile = pydag.htcondor.HTCondorSubmit(submitFile,
                                                script,
                                                **submitFileContent)
     submitFile.dump()
-    file_list = dataset_parser.get("Basics","file_list").split(":")
-    print "files to be read: " , file_list
+
+    folderlist = dataset_parser.get("Basics","folder_list")
+    filelist = dataset_parser.get("Basics","file_list")
+    filesperjob = args.filesperJob
+    outfolder = dataset_parser.get('Basics', 'out_folder')
+    file_bunches= []
+    
+    if folderlist == 'allinmcpath':
+        folderlist =  [subfolder for subfolder in os.listdir(basepath) \
+                       if os.path.isdir(os.path.join(basepath,subfolder))]
+    else:
+        folderlist = [folder.strip() for folder in folderlist.split(',')]
+
+    if not filelist == 'allinfolder':
+        filelist = filelist.split(',')
+    run_filelist = []
+    for i, folder in enumerate(folderlist):
+        print folder
+        for root, dirs, files in os.walk(os.path.join(basepath, folder)):
+            i3_files_all = [single_file for single_file in files if single_file[-6:]=='i3.bz2']
+            if not filelist == 'allinfolder':
+                i3_files = []
+                for single_file in i3_files_all:
+                    if np.any([fnmatch.fnmatch(single_file, syntax) for syntax in filelist]):
+                        i3_files.append(single_file)
+            else:
+                i3_files = i3_files_all
+            if len(i3_files)>0:
+                run_filelist.extend([os.path.join(root, single_file) for single_file in i3_files])
+        if filesperjob == -1:
+            with open(os.path.join(outfolder, 'File_{}.txt'.format(i)), 'w+') as f:
+                for line in run_filelist:
+                    f.write("%s \n" % line)
+            file_bunches.append('File_{}'.format(i))
+            run_filelist = []
+    if filesperjob != -1 :
+        run_filelist = [run_filelist[i:i+filesperjob] if (i+filesperjob) < len(run_filelist)\
+                        else run_filelist[i:] for i in np.arange(0, len(run_filelist), filesperjob)]
+        for i, single_filelist in enumerate(run_filelist):
+            with open(os.path.join(outfolder, 'File_{}.txt'.format(i)), 'w+') as f:
+                for line in run_filelist:
+                    f.write("%s \n" % line) 
+            file_bunches.append('File_{}'.format(i))
     nodes  = []
-    for i, filename in enumerate(file_list): #for i, a1, a2 in enumerate(itertools.product(args1, args2))
-        filename = filename.strip(".h5")
-        logfile = path+filename.replace('/','_').replace(':','_')+"_"+str(i)
+    for i, bunch in enumerate(file_bunches):
+        logfile = log_path+bunch
         dagArgs = pydag.dagman.Macros(LOGFILE=logfile,
-                                  PROJECT = args.project,
-                                  FOLDER = filename,
-                                  MAIN = args.main_config,
-                                  DATASET = args.dataset_config,
-                                  NUMFILES=args.num_files)
+                                      PATH = os.path.join(outfolder, '{}.txt'.format(bunch)),
+                                      DATASET = args.dataset_config)
         node = pydag.dagman.DAGManNode(i, submitFile)
         node.keywords["VARS"] = dagArgs
         nodes.append(node)
