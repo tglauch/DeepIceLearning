1de6f04298a562c6cc06fb7968d181997f715baf
Theo Glauch <theo.glauch@tum.de>
2017-09-08 08:24:20 +0200

    add new layers

--- functions.py
+++ functions.py
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import keras
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Activation, Flatten, Convolution2D,\
- BatchNormalization, MaxPooling2D,Convolution3D,MaxPooling3D, Merge
+    BatchNormalization, MaxPooling2D, Convolution3D, MaxPooling3D, Merge
 from keras import regularizers
 import os
 import numpy as np
@@ -33,6 +33,13 @@ def gpu_memory():
         if str(os.getpid()) in item and 'python' in item:
             ret = item.strip().split(' ')[-2]
     return float(ret[:-3])
+
+
+class MemoryCallback(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, log={}):
+        print(' \n RAM Usage {:.2f} GB \n \n'.format(
+            resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1e6))
+        os.system("nvidia-smi")
 
 
 def read_input_len_shapes(file_location, input_files, virtual_len=-1):
@@ -70,13 +77,6 @@ def read_input_len_shapes(file_location, input_files, virtual_len=-1):
         file_len.append(data_len)
         file_handler.close()
     return file_len
-
-
-class MemoryCallback(keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, log={}):
-        print(' \n RAM Usage {:.2f} GB \n \n'.format(
-            resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1e6))
-        os.system("nvidia-smi")
 
 
 def generator(batch_size, file_handlers, inds,
@@ -214,3 +214,33 @@ def read_NN_weights(args_dict, model):
 
     return model
 
+
+def residual_unit(x0):
+
+    x = Convolution3D(64, (1, 1, 1), border_mode="same", activation="relu")(x0)
+    x = Convolution3D(64, (3, 3, 3), border_mode="same", activation="relu")(x0)
+    return Merge.add([x, x0])
+
+
+def dense_block(x0, n=8):
+    """ Create a block of n densely connected pairs of convolutions """
+    for i in range(n):
+        x = Convolution3D(8, (3, 3, 3), padding='same', activation='relu')(x0)
+        x0 = Merge.concatenate([x0, x], axis=-1)
+    return x
+
+
+def inception_unit(x0):
+    x1 = Convolution3D(16, (1, 1, 1), padding='same', activation='relu')(x0)
+    x1 = Convolution3D(16, (3, 3, 3), padding='same', activation='relu')(x1)
+
+    x2 = Convolution3D(16, (1, 1, 1), padding='same', activation='relu')(x0)
+    x2 = Convolution3D(16, (5, 5, 5), padding='same', activation='relu')(x2)
+
+    x3 = Convolution3D(16, (1, 1, 1), padding='same', activation='relu')(x0)
+
+    x4 = MaxPooling3D((3, 3, 3), strides=(1, 1, 1), padding='same')(x0)
+    x4 = Convolution3D(64, (1, 1, 1), padding='same', activation='relu')(x4)
+
+
+    return Merge.concatenate([x1, x2, x3, x4], axis=1)
