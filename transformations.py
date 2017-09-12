d62a2d82b1eab44db18638b53c84e78d7f6906e2
Johannes <dschotze@gmail.com>
2017-09-12 15:35:02 +0200

    added zenith_to_binary trafo

--- transformations.py
+++ transformations.py
@@ -7,7 +7,6 @@ import numpy as np
 def identity(x):
     return x
 
-
 def centralize(x):
     if np.std(x)>0.:
         return ((x - np.mean(x)) / np.std(x))
@@ -22,3 +21,12 @@ def max_min_delta(x):
 
 def sort_input(x):
     return np.sort(np.ndarray.flatten(x))
+
+def zenith_to_binary(x):
+    """
+    returns boolean values for the zenith (0 or 1; up or down, > or < pi/2) as np.array.
+    """
+    ret = np.copy(x)
+    ret[ret < 1.5707963268] = 0.0
+    ret[ret > 1] = 1.0
+    return ret
