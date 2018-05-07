eb718eb6f850a061536326d971770174fb299c24
Kronmüller <ga38tul@slimfast.ktas.ph.tum.de>
2018-05-07 13:18:54 +0200

    plot_functions

---
+++ Plots/plot_functions_mk.py
@@ -0,0 +1,126 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import itertools
+
+
+
+
+
+def acc_loss_plot(acc_train, loss_train, acc_val, loss_val, title):
+    x = np.linspace(1, len(acc_train), num=len(acc_train), endpoint=True)
+    plt.plot(x, acc_train, color="#0099cc", label="acc of the training set")
+    plt.plot(x, loss_train, color='#35bcf8', label="loss of the training set")
+    plt.plot(x, acc_val, color='#6c1ba1', label="acc of the validation set")
+    plt.plot(x, loss_val, color='#af27cd', label="loss of the validation set")
+    plt.title(title)
+    #plt.set_ylim(0., 1.8)
+    plt.legend(bbox_to_anchor=(0.59, 0.97), loc=2, borderaxespad=0.)
+    plt.ylabel('loss & percentage')
+    plt.xlabel("epochs")
+
+
+
+def plot_confusion_matrix(cm, classes,title="",
+                          cmap=plt.cm.Blues):
+    """
+    This function plots the confusion matrix.
+    """
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(len(classes)), range(len(classes))):
+        plt.text(j, i, '{:.2f}'.format(cm[i, j]),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")  
+
+    plt.ylabel('Predicted label')
+    plt.xlabel('True label')
+    #plt.tight_layout()
+    #plt.show()
+
+
+def acc_vs_energy_plot(pred, true, energy, title, binnumber):
+    '''
+    inputs:
+    pred: predictions of the NN of the events
+    true: integer - what should the NN predict - number of class
+    energy: energys of the events
+       
+    '''
+
+    was_NN_right = [] #was_NN_right: Mask with 1 if prediction was right, if not 0
+    for i in xrange(0,len(pred)):
+        if np.argmax(pred[i]) == true:
+            was_NN_right.append(1)
+        else:
+            was_NN_right.append(0)
+    
+    
+    bins = np.linspace(np.min(np.log10(energy)), np.max(np.log10(energy)), binnumber)
+    valsTrue = np.histogram(np.log10(energy), weights=was_NN_right, bins=bins)[0]
+    valsAll = np.histogram(np.log10(energy), bins=bins)[0]
+    acc = 1.*valsTrue/valsAll
+
+
+    plt.plot(bins[:-1], acc, "x")
+    #plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.ylabel('accuracy')
+    plt.xlabel('log_10(E/GeV)')
+
+
+
+def summary_plot_binary_target(pred_target, MC_target, classes, energy, binnumber):
+    
+    # masking the lists to the labels
+    pred_for_class1 = pred_target[MC_target == 0]
+    E_for_class1 = energy[MC_target == 0]
+    pred_for_class2 = pred_target[MC_target == 1]
+    E_for_class2 = energy[MC_target == 1]
+    
+    # calculating values for the confusion matrix
+    absolute, xe,ye = np.histogram2d(np.argmax(pred_target, axis=1), MC_target, bins=(len(classes),len(classes)))
+    # that means that the prediction has to correspond to a event type 
+    res_true = absolute/np.sum(absolute,axis=0)
+    # normalization if the absolute values, so that the sum over PREDICTION is 1
+    # that means that a event hast to be predicted as one type 
+    res_pred = absolute/np.sum(absolute,axis=1).reshape(-1,1)
+    
+    # PLOT
+    fig = plt.figure(figsize=(16, 16))
+    ax1 = plt.subplot2grid((3, 2), (0, 0))
+    ax1 = acc_vs_energy_plot(pred_for_class1, 0, E_for_class1, "Accuracy vs. Energy for class {}".format(classes[0]), binnumber)
+
+    ax2 = plt.subplot2grid((3, 2), (0, 1))
+    ax2 = acc_vs_energy_plot(pred_for_class2, 1, E_for_class2, "Accuracy vs. Energy for class {}".format(classes[1]), binnumber)
+    
+    ax3 = plt.subplot2grid((3, 2), (1, 0))
+    bins = np.linspace(np.min(np.log10(E_for_class1)), np.max(np.log10(E_for_class1)), binnumber)
+    valsAll = np.histogram(np.log10(E_for_class1), bins=bins)[0]
+    ax3.semilogy(bins[:-1], valsAll)
+    ax3.set_title("Statistic of {} Events vs. Energy".format(classes[0]))
+    ax3.set_xlabel('log_10(E/GeV)')
+    ax3.set_ylabel('amount of events')
+    
+    ax4 = plt.subplot2grid((3, 2), (1, 1))
+    bins = np.linspace(np.min(np.log10(E_for_class2)), np.max(np.log10(E_for_class2)), binnumber)
+    valsAll = np.histogram(np.log10(E_for_class2), bins=bins)[0]
+    ax4.semilogy(bins[:-1], valsAll)
+    ax4.set_title("Statistic of {} Events vs. Energy".format(classes[1]))
+    ax4.set_xlabel('log_10(E/GeV)')
+    ax4.set_ylabel('amount of events')
+    
+    ax5 = plt.subplot2grid((3, 2), (2, 0))
+    plot_confusion_matrix(res_true, classes=classes, title='Confusion matrix normalized on MCTruth')
+ 
+    ax6 = plt.subplot2grid((3, 2), (2, 1))
+    plot_confusion_matrix(res_pred, classes=classes, title='Confusion matrix normalized on PREDICTION')
+
+
+
