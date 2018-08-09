d6212084e53315261476945d572f74d2b599fb4c
tglauch <theo.glauch@tum.de>
2018-08-09 13:11:32 +0200

    remove unused files

--- nn_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/
-
-python /scratch9/mkron/software/DeepIceLearning/neural_network.py --main_config /scratch9/mkron/software/DeepIceLearning/configs/main_mk.cfg --input all --model /scratch9/mkron/software/DeepIceLearning/networks/new_multi.py
