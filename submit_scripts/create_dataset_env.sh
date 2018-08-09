f92e1f6598bbee128919a01be67f9a48c0c0b9c1
Theo Glauch <theo.glauch@tum.de>
2018-08-09 15:43:31 +0200

    remove bug in env scripts

--- submit_scripts/create_dataset_env.sh
+++ submit_scripts/create_dataset_env.sh
@@ -5,5 +5,5 @@ eval `/cvmfs/icecube.opensciencegrid.org/py2-v3.0.1/setup.sh`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 SDIR="$(dirname "$DIR")"
 
-source /cvmfs/icecube.opensciencegrid.org/py2-v3.0.1/RHEL_7_x86_64/metaprojects/combo/stable/env-shell.sh python $("$SDIR/create_data_files.py") $@
+source /cvmfs/icecube.opensciencegrid.org/py2-v3.0.1/RHEL_7_x86_64/metaprojects/combo/stable/env-shell.sh python "$SDIR/create_data_files.py" $@
 
--- submit_scripts/nn_env.sh
+++ submit_scripts/nn_env.sh
@@ -4,6 +4,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 SDIR="$(dirname "$DIR")"
-python $("$SDIR/neural_network.py") $@
+python "$SDIR/neural_network.py" $@
 
 #python /scratch9/mkron/software/DeepIceLearning/neural_network.py --main_config /scratch9/mkron/software/DeepIceLearning/configs/main_mk.cfg --input all --model /scratch9/mkron/software/DeepIceLearning/Networks/classifikation_mk/new_multi.py
