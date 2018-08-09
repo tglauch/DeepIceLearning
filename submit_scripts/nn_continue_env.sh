3e5ba4a2ec87c7572f8ecc482f8089b1467fec1a
tglauch <theo.glauch@tum.de>
2018-08-09 08:25:01 -0500

    remove hard coded path in bash env scriptsg

--- submit_scripts/nn_continue_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/
-
-python /scratch9/mkron/software/DeepIceLearning/neutral_network.py --main_config /scratch9/mkron/software/DeepIceLearning/configs/main_mk.cfg --input all --model /scratch9/mkron/data/NN_out/run13/new_multi.py --continue /scratch9/mkron/data/NN_out/run13/ --load_weights /scratch9/mkron/data/NN_out/run13/best_val_loss.npy
--- submit_scripts/nn_env.sh
+++ submit_scripts/nn_env.sh
@@ -2,6 +2,8 @@
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/
 
-python /scratch9/tglauch/DeepIceLearning/neural_network.py $@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+SDIR="$(dirname "$DIR")"
+python $("$SDIR/neural_network.py") $@
 
 #python /scratch9/mkron/software/DeepIceLearning/neural_network.py --main_config /scratch9/mkron/software/DeepIceLearning/configs/main_mk.cfg --input all --model /scratch9/mkron/software/DeepIceLearning/Networks/classifikation_mk/new_multi.py
