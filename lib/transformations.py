0c2f43a2a942a4dfdfd5213eaa9449923494d9f0
tglauch <theo.glauch@tum.de>
2018-08-31 09:14:39 +0200

    cleanup

--- lib/transformations.py
+++ lib/transformations.py
@@ -5,62 +5,74 @@ import numpy as np
 from keras.utils import to_categorical
 from scipy.stats import norm
 
-def identity(x,r_vals=None):
+
+def identity(x, r_vals=None):
     return x
 
+
 def centralize(x):
-    if np.std(x)>0.:
+    if np.std(x) > 0.:
         return ((x - np.mean(x)) / np.std(x))
     else:
         return (x - np.mean(x))
+
+
 def waveform_offset(x):
-    return (x-10000)/400
+    return (x - 10000) / 400
+
 
 def max(x):
     return np.amax(x)
 
+
 def max_min_delta(x):
-    return np.max(x)-np.min(x)
+    return np.max(x) - np.min(x)
+
 
 def shift_min_to_zero(x):
-    return x-np.amin(x)
+    return x - np.amin(x)
+
 
 def sort_input(x):
     return np.sort(np.ndarray.flatten(x))
+
 
 def plus_one_log10(x):
     tmp = x + 1.
     return np.log10(tmp)
 
+
 def zenith_prep(x, r_vals=None):
-    #if r_vals is not None:
-    #   print x, r_vals['zenith']
-    x =  x / np.pi
+    x = x / np.pi
     return x
 
 
 def log_handle_zeros_flatten_top30(x):
-    #tmp = np.where(x != 0, np.log10(x), 0)
-    return np.sort(np.ndarray.flatten(np.log10(1.+x)))[-30:]
+    return np.sort(np.ndarray.flatten(np.log10(1. + x)))[-30:]
+
 
 def log_handle_zeros(x):
     return np.where(x != 0, np.log10(x), 0)
 
+
 def sort_input_and_top20(x):
     return np.sort(np.ndarray.flatten(x))[-20:]
 
+
 def smeared_one_hot_encode_logbinned(E):
     width = 0.16
-    bins=np.linspace(3,7,50)
-    gauss = norm(loc = np.log10(E), scale = width)
+    bins = np.linspace(3, 7, 50)
+    gauss = norm(loc=np.log10(E), scale=width)
     smeared_hot_output = gauss.pdf(bins)
-    return smeared_hot_output/np.sum(smeared_hot_output)
+    return smeared_hot_output / np.sum(smeared_hot_output)
+
 
 def one_hot_encode_logbinned(x):
-    bins=np.linspace(3,7,50)
+    bins = np.linspace(3, 7, 50)
     bin_indcs = np.digitize(np.log10(x), bins)
     one_hot_output = to_categorical(bin_indcs, len(bins))
     return one_hot_output
+
 
 def zenith_to_binary(x):
     """
@@ -70,6 +82,7 @@ def zenith_to_binary(x):
     ret[ret < 1.5707963268] = 0.0
     ret[ret > 1] = 1.0
     return ret
+
 
 def time_prepare(x):
     """
@@ -84,13 +97,14 @@ def time_prepare(x):
     ret[ret == np.inf] = replace_with
     return ret
 
+
 def oneHotEncode_EventType_simple(x):
     """
-    This function one hot encodes the input for the event types cascade, tracks, doubel-bang
+    This function one hot encodes the input for the event 
+    types cascade, tracks, doubel-bang
     """
     # define universe of possible input values
     onehot_encoded = []
-    # universe has to defined depending on the problem, in this implementation integers are neccesary
     universe = [1, 2, 3]
     for i in range(len(universe)):
         if x == universe[i]:
@@ -100,13 +114,13 @@ def oneHotEncode_EventType_simple(x):
         onehot_encoded.append(value)
     return onehot_encoded
 
+
 def oneHotEncode_EventType_noDoubleBang_simple(x):
     """
     This function one hot encodes the input
     """
     # define universe of possible input values
     onehot_encoded = []
-    # universe has to defined depending on the problem, in this implementation integers are neccesary
     universe = [1, 2, 3]
     for i in range(len(universe)):
         if x == universe[i]:
@@ -118,11 +132,13 @@ def oneHotEncode_EventType_noDoubleBang_simple(x):
         onehot_encoded = [1.0, 0.0, 0.0]
     return onehot_encoded[:-1]
 
+
 def log_of_sum(x):
-    return np.log10(np.sum(x)+0.0001)
+    return np.log10(np.sum(x) + 0.0001)
+
 
 def max_min_delta_log(x):
-    return np.log10(np.max(x)-np.min(x))
+    return np.log10(np.max(x) - np.min(x))
 
 
 def oneHotEncode_01(x, r_vals=None):
@@ -131,7 +147,6 @@ def oneHotEncode_01(x, r_vals=None):
     """
     # define universe of possible input values
     onehot_encoded = []
-    # universe has to defined depending on the problem, in this implementation integers are neccesary
     universe = [0, 1]
     for i in range(len(universe)):
         if x == universe[i]:
@@ -144,11 +159,13 @@ def oneHotEncode_01(x, r_vals=None):
 
 def oneHotEncode_EventType_exact(x, r_vals=None):
     """
-    This function one hot encodes the input for the event types cascade, tracks, doubel-bang
+    This function one hot encodes the input for the event
+    types cascade, tracks, doubel-bang
     """
     # define universe of possible input values
     onehot_encoded = []
-    # universe has to defined depending on the problem, in this implementation integers are neccesary
+    # universe has to defined depending on the problem,
+    # in this implementation integers are neccesary
     universe = [0, 1, 2, 3, 4, 5, 6]
     for i in range(len(universe)):
         if x == universe[i]:
@@ -158,66 +175,64 @@ def oneHotEncode_EventType_exact(x, r_vals=None):
         onehot_encoded.append(value)
     return onehot_encoded
 
-def oneHotEncode_EventType(x, r_vals=None):
+
+def oneHotEncode_3_evtypes(x, r_vals=None):
     """
-    This function one hot encodes the input for the event types cascade, tracks, doubel-bang
+    This function one hot encodes the input for the event
+    types cascade, tracks, doubel-bang
     """
+
     # define universe of possible input values
-    #fail = [0., 0., 0.]
     cascade = [1., 0., 0.]
     track = [0., 1., 0.]
     s_track = [0., 0., 1.]
     # map x to possible classes
-    mapping = {0:cascade, 1:cascade, 2:track, 3:track, 4:track, 5:doublebang, 6:doublebang, 7:cascade, 8:track, 9:cascade}
+    mapping = {0: cascade, 1: cascade, 2: track, 3: s_track, 4: track,
+               5: cascade, 6: cascade, 7: cascade, 8: track, 9: cascade}
     return mapping[x]
 
-def oneHotEncode_EventType_stratingTrack(x, r_vals=None):
+
+def oneHotEncode_db(x, r_vals=None):
     """
-    This function one hot encodes the input for the event types cascade, tracks, doubel-bang, starting tracks
+    This function one hot encode for event type  doubel-bang, no-double bang
     """
-    #print type(list(r_vals))
-    #print "r_vals: {}".format(r_vals)
-    #print "x: {}".format(x)
     # define universe of possible input values
-    #fail = [0., 0., 0., 0.]
+
+    ndoublebang = [1., 0.]
+    doublebang = [0., 1.]
+
+    mapping = {0: ndoublebang, 1: ndoublebang, 2: ndoublebang, 3: ndoublebang,
+               4: ndoublebang, 5: doublebang, 6: doublebang, 7: ndoublebang,
+               8: ndoublebang, 9: ndoublebang}
+    return mapping[x]
+
+
+def oneHotEncode_4_evtypes(x, r_vals=None):
+    """
+    This function one hot encodes the input for the event types 
+    cascade, tracks, doubel-bang, starting tracks
+    """
+
     cascade = [1., 0., 0., 0.]
     track = [0., 1., 0., 0.]
     doublebang = [0., 0., 1., 0.]
-    startingTrack = [0., 0., 0., 1.]
+    s_track = [0., 0., 0., 1.]
     # map x to possible classes
-    mapping = {0:cascade, 1:cascade, 2:track, 3:startingTrack, 4:track, 5:doublebang, 6:doublebang, 7:cascade, 8:track, 9:cascade}
+    mapping = {0: cascade, 1: cascade, 2: track, 3: s_track, 4: track,
+               5: doublebang, 6: doublebang, 7: cascade, 8: track, 9: cascade}
     return mapping[x]
+
 
 def oneHotEncode_Starting_padding(x, r_vals):
     pos = [r_vals[14], r_vals[15], r_vals[16]]
     dir = [r_vals[17], r_vals[18], r_vals[19]]
     gcdfile = "/cvmfs/icecube.opensciencegrid.org/data/GCD/GeoCalibDetectorStatus_2013.56429_V0.i3.gz"
     padding = 100
-    surface = icecube.MuonGun.ExtrudedPolygon.from_file(gcdfile, padding= padding)
+    surface = icecube.MuonGun.ExtrudedPolygon.from_file(gcdfile,
+                                                        padding=padding)
     intersections = surface.intersection(pos, dir)
     if intersections.first <= 0 and intersections.second > 0:
         starting = 0  # starting event
     else:
         starting = 1  # through-going or stopping event
     return starting
-
-def oneHotEncode_DB(x, r_vals=None):
-    """
-    This function one hot encode for event type  doubel-bang, no-double bang
-    """
-    #print type(list(r_vals))
-    #print "r_vals: {}".format(r_vals)
-    #print "x: {}".format(x)
-    # define universe of possible input values
-    fail = [0., 0.]
-    ndoublebang = [1., 0.]
-    doublebang = [0., 1.,]
-    
-    # map x to possible classes
-    if x == 5: #Double Bang
-        onehot_encoded = doublebang
-    elif x in [0,1,2,3,4,6,7,8,9]:
-        onehot_encoded = ndoublebang
-    else:
-        onehot_encoded = fail
-    return onehot_encoded
--- submit_scripts/create_dataset_dagman.py
+++ submit_scripts/create_dataset_dagman.py
@@ -7,8 +7,8 @@ import argparse
 import time
 import numpy as np
 from configparser import ConfigParser
-import fnmatch
 import cPickle as pickle
+
 
 def parseArguments():
     # Create argument parser
@@ -19,16 +19,12 @@ def parseArguments():
         type=str, default='create_dataset.cfg')
     parser.add_argument(
         "--filesperJob",
-        help="n files per job ", default=-1,
+        help="n files per job ", default=50,
         type=int)
     parser.add_argument(
         "--name",
         help="Name for the Dagman Files",
         type=str, default='create_dataset')
-    parser.add_argument(
-        "--create_script",
-        help="Different script to execute",
-        type=str, default='submit_scripts/create_dataset_env.sh')
     parser.add_argument(
         "--request_RAM",
         help="amount of RAM in GB",
@@ -47,9 +43,7 @@ if __name__ == '__main__':
     args = parseArguments()
     for a in args.__dict__:
         print str(a) + ": " + str(args.__dict__[a])
-    #main_parser = ConfigParser()
     dataset_parser = ConfigParser()
-    #main_parser.read(config_file)
     dataset_parser.read(args.dataset_config)
     print"############################################\n "
 
@@ -58,39 +52,40 @@ if Resc == '':
     today = str(datetime.datetime.now()).\
         replace(" ", "-").split(".")[0].replace(":", "-")
 
-    PROCESS_DIR = dataset_parser.get("Basics", "dagman_folder") + "/" + today + "/"
+    PROCESS_DIR = os.path.join(dataset_parser.get("Basics", "dagman_folder"),
+                               today)
     if not os.path.exists(PROCESS_DIR):
         os.makedirs(PROCESS_DIR)
 
     WORKDIR = os.path.join(PROCESS_DIR, "jobs/")
     script = os.path.join(
-        #"/data/user/mkronmueller/code/DeepIceLearning", 
-        dataset_parser.get("Basics", "thisfolder"),\
-        args.__dict__["create_script"])
-    print('Submit Script:')
-    print(script)
+        dataset_parser.get("Basics", "thisfolder"),
+        'submit_scripts/create_dataset_env.sh')
+    print('Submit Script:\n {}'.format(script))
+
     dag_name = args.__dict__["name"]
     dagFile = os.path.join(
         WORKDIR, "job_{}.dag".format(dag_name))
     submitFile = os.path.join(WORKDIR, "job_{}.sub".format(dag_name))
     if not os.path.exists(WORKDIR):
         os.makedirs(WORKDIR)
-        print "Created New Folder in: {}".format(WORKDIR)
+        print("Created New Folder in: {}".format(WORKDIR))
+
     log_path = os.path.join(PROCESS_DIR, "logs/{}/".format(dag_name))
     if not os.path.exists(log_path):
         os.makedirs(log_path)
-        print "Created New Folder in: {}".format(log_path)
-    print "Write Dagman Files to: {}".format(submitFile)
+        print("Created New Folder in: {}".format(log_path))
+
+    print("Write Dagman Files to: {}".format(submitFile))
     RAM_str = "{} GB".format(args.__dict__["request_RAM"])
-    #arguments = " --filelist $(PATH) --dataset_config $(DATASET) "
     arguments = " --filelist $(PATHs) --dataset_config $(DATASET) "
     submitFileContent = {"universe": "vanilla",
                          "notification": "Error",
                          "log": "$(LOGFILE).log",
                          "output": "$(LOGFILE).out",
                          "error": "$(LOGFILE).err",
-	#		 "Requirements" : "HAS_CVMFS_icecube_opensciencegrid_org",
-                         "Requirements" : '(Machine != "n-15.icecube.wisc.edu")',
+	#		             "Requirements" : "HAS_CVMFS_icecube_opensciencegrid_org",
+    #                    "Requirements" : '(Machine != "n-15.icecube.wisc.edu")',
                          "request_memory": RAM_str,
                          "arguments": arguments}
     submitFile = pydag.htcondor.HTCondorSubmit(submitFile,
@@ -98,29 +93,19 @@ if Resc == '':
                                                **submitFileContent)
     submitFile.dump()
     folderlist = dataset_parser.get("Basics", "folder_list")
-    basepath = []
-    for i in xrange(3):  # zum testen
-    #for i in xrange(len(args['filelist'])):
-        a = "MC_path" + str(i)
-        basepath.append(str(dataset_parser.get('Basics', a)))
-
+    basepath = [dataset_parser['Basics'][key] for key in
+                dataset_parser['Basics'].keys() if 'mc_path' in key]
     filelist = dataset_parser.get("Basics", "file_list")
-    filesperjob = args.filesperJob
+    filesjob = args.filesperJob
     file_bunches = []
 
     if folderlist == 'allinmcpath':
         folderlist = []
-        for i in xrange(len(basepath)):
-            check_sys = os.path.isdir(os.path.join(basepath[i], "00000-00999/clsim-base-4.0.3.0.99_eff"))
-            print "Check for systematic dataset: {}".format(check_sys)
-            if check_sys:
-                a = [subfolder + "/clsim-base-4.0.3.0.99_eff" for subfolder in os.listdir(basepath[i])
-                         if os.path.isdir(os.path.join(basepath[i], subfolder))]
-            else:
-                a = [subfolder for subfolder in os.listdir(basepath[i])
-                         if os.path.isdir(os.path.join(basepath[i], subfolder))]
+        for p in basepath:
+            a = [subfolder for subfolder in os.listdir(p)
+                 if os.path.isdir(os.path.join(p, subfolder))]
             if "logs" in a:
-                a.remove("logs") 
+                a.remove("logs")
             if "job_stats.json.gz" in a:
                 a.remove("job_stats.json.gz")
             folderlist.append(a)
@@ -129,97 +114,55 @@ if Resc == '':
 
     if not filelist == 'allinfolder':
         filelist = filelist.split(',')
-#####################################################################
-    longest = len(folderlist[0])
-    factor_list = []
+
     for j, sim in enumerate(folderlist):
-        if len(folderlist[j]) > longest:
-             longest = len(folderlist[j])
-    for j, sim in enumerate(folderlist):
-        if len(folderlist[j]) < longest:
-             factor = longest / len(folderlist[j])
-             factor_list.append(factor * 10)
-        else:
-             factor_list.append(10)
-    factor_list = factor_list
-    print factor_list
-#####################################################################
-    for j, sim in enumerate(folderlist):
-        outfolder = dataset_parser.get('Basics', 'out_folder') +"/filelists/dataset" + str(j)
+        outfolder = os.path.join(dataset_parser.get('Basics', 'out_folder'),
+                                 "filelists/dataset",
+                                 str(j))
         if not os.path.exists(outfolder):
             os.makedirs(outfolder)
         run_filelist = []
         for ii, folder in enumerate(folderlist[j]):
-            for root, dirs, files in os.walk(os.path.join(basepath[j], folder)):
-                i3_files_all = [single_file for single_file in files
-                                if single_file[-6:] == 'i3.bz2']
+            for root, dirs, files in os.walk(os.path.join(basepath[j],
+                                                          folder)):
+                i3_files_all = [s_file for s_file in files
+                                if s_file[-6:] == 'i3.bz2']
                 if not filelist == 'allinfolder':
-                    i3_files = []
-                    for single_file in i3_files_all:
-                        if np.any(
-                            [fnmatch.fnmatch(single_file, syntax)
-                             for syntax in filelist]):
-                            i3_files.append(single_file)
+                    i3_files = [f for f in filelist if f in i3_files_all]
                 else:
                     i3_files = i3_files_all
                 if len(i3_files) > 0:
                     b = [os.path.join(root, single_file)
-                                         for single_file in i3_files]
+                         for single_file in i3_files]
                     run_filelist.extend(b)
-#################################################################################################
-            #run_filelist = run_filelist[:5]
-#################################################################################################
-            #if filesperjob == -1:
-            #    if factor_list[j] == 10:
-            #        with open(os.path.join(outfolder,
-            #                               'File_{}.pickle'.format(ii)), 'w+') as f:
-            #            pickle.dump(run_filelist, f)
-            #        #file_bunches.append('File_{}'.format(i))
-            #        run_filelist = []
-            #    else:
-            #        size_chunk = len(run_filelist)/factor_list[j]+1
-            #        for k in xrange(factor_list[j]):
-            #            run_filelist_k = run_filelist[k*(size_chunk) : ((k+1)*size_chunk)-1]
-            #            with open(os.path.join(outfolder,
-            #                                   'File_{}.pickle'.format(ii*factor_list[j]+k)), 'w+') as f:
-            #                pickle.dump(run_filelist_k, f)
-            #        run_filelist = []
-            if filesperjob == -1:
-                filesjob = 50
-                run_filelist = [run_filelist[i:i + filesjob] for i in np.arange(
-                                    0, len(run_filelist), filesjob+1)] 
-#                run_filelist = [run_filelist[i:i + filesjob]
-#                                if (i + filesjob) < len(run_filelist)
-#                                else run_filelist[i:] for i in np.arange(
-#                                    0, len(run_filelist), filesjob)]
-                for numberInRunFilelist, single_filelist in enumerate(run_filelist):
-                    #print "ii {}".format(ii)
-                    #print "number for pickle {}".format(ii*20+numberInRunFilelist)
-                    #print "Number intern {}".format(numberInRunFilelist)
-		    #print "singe_filelist {}".format(single_filelist)
-                    with open(os.path.join(outfolder,
-                                           'File_{}.pickle'.format(ii*20+numberInRunFilelist)), 'w+') as f:
-                        pickle.dump(single_filelist, f)
-                run_filelist = []   
-                #file_bunches.append('File_{}'.format(i))
-        #list_file_bunches.append(file_bunches)
+            run_filelist = [run_filelist[i:i + filesjob] for i
+                            in np.arange(0, len(run_filelist), filesjob + 1)]
+            for numberInRunFilelist, single_filelist in enumerate(run_filelist):
+                with open(os.path.join(outfolder,
+                                       'File_{}.pickle'.format(ii * 20 + numberInRunFilelist)), 'w+') as f:
+                    pickle.dump(single_filelist, f)
+            run_filelist = []
+
+
 #################### if Job number is set by hand ################
     misty = 0
-    while misty <= 999:  # bad quick fix
-    #while misty <= 199: # bad quick fix
+    while misty <= 999:  # bad quick fix ###### AAAAAAAAAAAHHHH
         file_bunches.append('File_{}'.format(misty))
         misty += 1
 ###################################################################
+
     nodes = []
     for i, bunch in enumerate(file_bunches):
         logfile = log_path + bunch
-        PATH = str()
+        PATH = ''
         for k in xrange(len(basepath)):
-            PATH = PATH + os.path.join(dataset_parser.get('Basics', 'out_folder') +"/filelists/dataset" + str(k),'{}.pickle'.format(bunch)) +" "
+            PATH = PATH +\
+                os.path.join(dataset_parser.get('Basics', 'out_folder'),
+                             "filelists/dataset",
+                             str(k),
+                             '{}.pickle'.format(bunch)) + " "
         dagArgs = pydag.dagman.Macros(LOGFILE=logfile,
                                       PATHs=PATH,
-                                      #PATH=os.path.join(outfolder,
-                                      #                  '{}.pickle'.format(bunch)),
                                       DATASET=args.dataset_config)
         node = pydag.dagman.DAGManNode(i, submitFile)
         node.keywords["VARS"] = dagArgs
